f1e0d2c357a1b48c4845e9aa2e05c970b0257a85
João Vitor Miranda Silva Borges <borgesjoaovitor444@gmail.com>
2024-04-10 18:42:41 -0300

    Novos Alias

--- alias.sh
+++ alias.sh
@@ -7,3 +7,5 @@ alias backup='cd $SCRIPTS && ./backup_programação.sh && cd --'
 alias atualizar='cd $HOME && source .bashrc && cd --'
 
 alias desmontar='sudo umount UUID=2CB0-554D'
+
+alias aprender_c='bash -c "$(curl https://grademe.fr)"'
