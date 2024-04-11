9398c2d114c72eeeed97d74b267d2ca1de0090e1
Jovi445 <75222136+jovi445@users.noreply.github.com>
2024-04-11 12:03:50 -0300

    Atualizar o alias.sh

--- alias.sh
+++ alias.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Ambiente e Scripts
+
 alias shellscripts='code $SCRIPTS'
 
 alias backup='cd $SCRIPTS && ./backup_programação.sh && cd --'
@@ -7,5 +9,7 @@ alias backup='cd $SCRIPTS && ./backup_programação.sh && cd --'
 alias atualizar='cd $HOME && source .bashrc && cd --'
 
 alias desmontar='sudo umount UUID=2CB0-554D'
+
+# Scripts Externos
 
 alias aprender_c='bash -c "$(curl https://grademe.fr)"'
