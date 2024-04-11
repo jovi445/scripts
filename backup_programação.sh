8f1dc8330c3cf251d7fc424a2c1d6956ee7a513c
Jovi445 <75222136+jovi445@users.noreply.github.com>
2024-04-11 12:02:13 -0300

    Atualizar o backup_programação.sh

--- backup_programação.sh
+++ backup_programação.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# Variáveis de Ambiente
+
 DATE=$(date +"%Y%m%d_%H%M%S")
 BACKUP_DIR="/media/$USER/Ventoy/Backups"
 SOURCE_DIR="$HOME/Documentos/Programação"
 
+# Compactação
+
 tar -cvzf "$BACKUP_DIR/backup_programação_$DATE.tar.gz" -C "$SOURCE_DIR" . >/dev/null 2>&1
+
+# Resultado
 
 if [ $? -eq 0 ]; then
     echo "========================================================================="
