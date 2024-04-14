f4a879b4f1f81916237e0351ecadf79b967548b4
João Vitor <borgesjoaovitor444@gmail.com>
2024-04-14 12:35:37 -0300

    Pequenos Tweaks

--- alias.sh
+++ alias.sh
@@ -6,7 +6,7 @@ alias shellscripts='code $SCRIPTS && cd --'
 
 alias backup='cd $SCRIPTS && ./backup_programação.sh && cd --'
 
-alias atualizar='cd $HOME && source .bashrc && cd -- && echo ".bashrc atualizado com sucesso!!"'
+alias atualizar='cd $HOME && source .bashrc && cd -- && echo "Ambiente atualizado com sucesso!!"'
 
 alias desmontar='umount UUID=2CB0-554D && echo "Dispositivo desmontado com Sucesso!!"'
 
