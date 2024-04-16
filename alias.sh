997fe8bcc2275c3ab782f679f8cb7f4bfa70aaa0
João Vitor <borgesjoaovitor444@gmail.com>
2024-04-16 14:23:26 -0300

    Removendo interações desnecessárias

--- alias.sh
+++ alias.sh
@@ -6,9 +6,9 @@ alias shellscripts='code $SCRIPTS && cd --'
 
 alias backup='cd $SCRIPTS && ./backup_programação.sh && cd --'
 
-alias atualizar='cd $HOME && source .bashrc && cd -- && echo "Ambiente atualizado com sucesso!!"'
+alias atualizar='cd $HOME && source .bashrc && cd --'
 
-alias desmontar='umount UUID=2CB0-554D && echo "Dispositivo desmontado com Sucesso!!"'
+alias desmontar='umount UUID=2CB0-554D'
 
 # Scripts Externos
 
