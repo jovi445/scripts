b2d66ed9c9fdd00c6a050a093536ed6fdbf24fc6
João Vitor Miranda Silva Borges <borgesjoaovitor444@gmail.com>
2024-05-07 19:53:39 -0300

    Removendo aliases desnecessários

--- alias.sh
+++ alias.sh
@@ -6,12 +6,6 @@ alias shellscripts='code $SCRIPTS && cd --'
 
 alias backup-pendrive='cd $SCRIPTS && ./backup_programação.sh && cd --'
 
-alias backup-servidor='scp -rf $HOME/Documentos/Programação joaov@192.168.100.7:/home/joaov'
-
 alias atualizar='cd $HOME && source .bashrc && cd --'
 
 alias desmontar='umount UUID=2CB0-554D'
-
-# Scripts Externos
-
-alias aprender_c='bash -c "$(curl https://grademe.fr)"'
