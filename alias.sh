6fdfe701328d9240f8a8347d7f74270d4a309628
João Vitor Miranda Silva Borges <borgesjoaovitor444@gmail.com>
2024-05-16 18:36:34 -0300

    Removendo aliases desnecessários

--- alias.sh
+++ alias.sh
@@ -6,8 +6,4 @@ alias shellscripts='code $SCRIPTS && cd --'
 
 alias backup='cd $SCRIPTS && ./backup_programação.sh && cd --'
 
-alias atualizar-bash='cd $HOME && source .bashrc && cd --'
-
-alias atualizar-zsh='cd $HOME && source .zshrc && cd --'
-
 alias desmontar='umount UUID=2CB0-554D'
