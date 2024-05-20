9b9667bb7217590df6432193294a2012d17b9f65
João Vitor Miranda Silva Borges <borgesjoaovitor444@gmail.com>
2024-05-20 16:28:29 -0300

    Adicionando Profiles

--- profile.sh
+++ profile.sh
@@ -7,3 +7,5 @@ export SCRIPTS=$HOME/Documentos/Programação/Scripts
 export PROJETOS=$HOME/Documentos/Programação/Projetos
 
 export ESCOLA=$HOME/Documentos/Programação/Escola
+
+export HOME_SERVER='192.168.100.7'
