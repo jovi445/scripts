43b1820f68d82a6ac785dd9c72e21563a34799a2
João Vitor Miranda Silva Borges <borgesjoaovitor444@gmail.com>
2024-06-18 17:44:14 -0300

    Removendo linhas inúteis

--- alias.sh
+++ alias.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
-# Ambiente e Scripts Internos
+## Ambiente e Scripts Internos
 
 alias shellscripts='code $SCRIPTS && cd --'
+
+#Backup
 
 alias backup='cd $SCRIPTS && ./backup_programação.sh && cd --'
 
 alias desmontar='umount UUID=2CB0-554D'
+
+#Teclado
+
+alias ligar-led='xset led named "Scroll Lock"'
+
+alias desligar-led='xset -led named "Scroll Lock"'
--- profile.sh
+++ profile.sh
@@ -7,5 +7,3 @@ export SCRIPTS=$HOME/Documentos/Programação/scripts
 export PROJETOS=$HOME/Documentos/Programação/Projetos
 
 export ESCOLA=$HOME/Documentos/Programação/Escola
-
-export HOME_SERVER='192.168.100.7'
