d3af65aed7480e44ae6510552fc4d5b391cfcfb3
João Vitor Miranda Silva Borges <borgesjoaovitor444@gmail.com>
2024-06-18 18:52:17 -0300

    Removendo alias teclado

--- alias.sh
+++ alias.sh
@@ -9,9 +9,3 @@ alias shellscripts='code $SCRIPTS && cd --'
 alias backup='cd $SCRIPTS && ./backup_programação.sh && cd --'
 
 alias desmontar='umount UUID=2CB0-554D'
-
-#Teclado
-
-alias ligar-led='xset led named "Scroll Lock"'
-
-alias desligar-led='xset -led named "Scroll Lock"'
