3e7a9bcee804964bc8a11041c189a395928d7967
João Vitor Miranda Silva Borges <borgesjoaovitor444@gmail.com>
2024-06-26 15:56:39 -0300

    Melhorias

--- alias.sh
+++ alias.sh
@@ -12,6 +12,6 @@ alias desmontar='umount UUID=2CB0-554D'
 
 #Leds Teclado
 
-alias ligar-led='sudo sh -c 'echo 1 > /sys/class/leds/input18::scrolllock/brightness''
+alias ligar-led='sudo sh -c '\''echo 1 > /sys/class/leds/input18::scrolllock/brightness'\'
 
-alias desligar-led='sudo sh -c 'echo 0 > /sys/class/leds/input18::scrolllock/brightness''
+alias desligar-led='sudo sh -c '\''echo 0 > /sys/class/leds/input18::scrolllock/brightness'\'
