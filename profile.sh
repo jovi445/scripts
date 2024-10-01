60a734bc79b9e161758c7e00cdca4366ae3a31b0
João Vitor <borgesjoaovitor444@gmail.com>
2024-10-01 20:50:51 -0300

    curso var

--- profile.sh
+++ profile.sh
@@ -7,3 +7,5 @@ export SCRIPTS=$HOME/Documentos/Programação/scripts
 export PROJETOS=$HOME/Documentos/Programação/Projetos
 
 export ESCOLA=$HOME/Documentos/Programação/Escola
+
+export CURSO=$HOME/Documentos/Programação/Curso
