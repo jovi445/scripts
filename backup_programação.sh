521bb7b731b221970ed84616a7badab116d1092f
João Vitor <borgesjoaovitor444@gmail.com>
2024-10-03 16:24:06 -0300

    Simplificando o script de backup

--- backup_programação.sh
+++ backup_programação.sh
@@ -9,15 +9,3 @@ SOURCE_DIR="$HOME/Documentos/Programação"
 # Compactação
 
 tar -cvzf "$BACKUP_DIR/backup_programação_$DATE.tar.gz" -C "$SOURCE_DIR" . >/dev/null 2>&1
-
-# Resultado
-
-if [ $? -eq 0 ]; then
-    echo "========================================================================="
-    echo "Backup de $SOURCE_DIR concluído com sucesso!"
-    echo "========================================================================="
-else
-    echo "========================================================================="
-    echo "Verifique se o dispositivo $BACKUP_DIR está montado!"
-    echo "========================================================================="
-fi
